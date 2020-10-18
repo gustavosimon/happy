eb3638a8b1b62725d17328300c24abb40f69f1a4
gustavosimon <gustavo.andre.simon@hotmail.com>
2020-10-18 11:30:42 -0300

    Finalized the backend, developed the image and orphanages models and views

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,16 @@
 import { Router } from "express";
 
+import multer from "multer";
+import multerConfig from "./config/upload";
+
 import OrphanagesController from "./controllers/OrphanagesController";
 
 const routes = Router();
 
+const upload = multer(multerConfig);
+
 routes.post("/orphanages", OrphanagesController.create);
+routes.get("/orphanages", OrphanagesController.index);
+routes.get("/orphanages/:id", upload.array("images"), OrphanagesController.show);
 
 export default routes;
