7044d5d778eadf4643a21d85d3a58ad789eea8ee
gustavosimon <gustavo.andre.simon@hotmail.com>
2020-12-06 14:17:33 -0300

    Create the sidebar component

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -33,24 +33,22 @@ function OrphanagesMap() {
           <span>Rio Grande do Sul</span>
         </footer>
       </aside>
-
       <Map
         center={[-29.5919468, -51.1480857]}
         zoom={15}
         style={{ width: "100%", height: "100%" }}
       >
         <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-
         <Marker
             position={[-29.5919468, -51.1480857]}
             icon={mapIcon}
         >
-            <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup" >
-               Lar das meninas
-               <Link to="orphanages/1">
-                <FiArrowRight size={20} color="#fff" />
-               </Link>
-            </Popup>
+          <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup" >
+              Lar das meninas
+              <Link to="orphanages/1">
+              <FiArrowRight size={20} color="#fff" />
+              </Link>
+          </Popup>
         </Marker>
       </Map>
       <Link to="/orphanages/create" className="create-orphanage">
